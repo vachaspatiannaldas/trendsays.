e132709b093667a835bffee58016f0f4c478a78a
unknown <annaldasvachaspati@gmail.com>
2024-04-10 20:58:18 +0530

    changes

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,8 +1,8 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = 'http://localhost:8000';
-
+// const url = 'http://localhost:8000';
+const url = window.location.origin;
 
 let gfs, gridfsBucket;
 const conn = mongoose.connection;
--- index.js
+++ index.js
@@ -8,14 +8,23 @@ import path from 'path';
 import Connection from './database/db.js';
 import Router from './routes/route.js';
 
+const app = express();
+
+const corsConfig ={
+    origin: "*",
+    credential: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+};
+
+app.options("", cors(corsConfig));
+app.use(cors(corsConfig));
+
 dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', Router);
